dd625c0f905a1b166a77b7da2ec3ec1eabc7bd64
zhicong xie <1392438023@qq.com>
2024-12-03 16:28:04 +0800

    upload web logic

--- src/test/java/com/zhiCong/plaform/Base/Config/WebDriverConfig.java
+++ src/test/java/com/zhiCong/plaform/Base/Config/WebDriverConfig.java
@@ -3,26 +3,25 @@ package com.zhiCong.Plaform.Base.Config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class WebDriverConfig {
 
-  private static WebDriver webDriver;
-  private static WebDriverConfig webDriverConfig;
+    private static WebDriver webDriver;
+    private static WebDriverConfig webDriverConfig;
 
-  public static WebDriverConfig getInstance() {
-    if (webDriverConfig == null) {
-      webDriverConfig = new WebDriverConfig();
+    public static WebDriverConfig getInstance() {
+        if (webDriverConfig == null) {
+            webDriverConfig = new WebDriverConfig();
+        }
+        return webDriverConfig;
     }
-    return webDriverConfig;
-  }
 
-  private WebDriverConfig() {
-    System.setProperty(
-        "webdriver.chrome.driver",
-        "/Users/automatiautomationon/Downloads/chromedriver-mac-x64/chromedriver");
-    webDriver = new ChromeDriver();
-  }
+    private WebDriverConfig() {
+        System.setProperty("webdriver.chrome.driver","/Users/automatiautomationon/Downloads/chromedriver-mac-x64/chromedriver");
+        webDriver = new ChromeDriver();
+    }
 
-  public static WebDriver getDriver() {
-    return webDriver;
-  }
+    public static WebDriver getDriver() {
+        return webDriver;
+    }
 }
