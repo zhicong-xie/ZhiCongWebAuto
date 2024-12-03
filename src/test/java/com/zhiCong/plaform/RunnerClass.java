318e46d023da01a5c2b7b46853d508c66b80f72f
zhicong xie <zhicong.zc.xie@gtomato.com>
2024-12-03 16:43:23 +0800

    upload web logic

--- src/test/java/com/zhiCong/plaform/RunnerClass.java
+++ src/test/java/com/zhiCong/plaform/RunnerClass.java
@@ -2,7 +2,15 @@ package com.zhiCong.Plaform;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.presentation.PresentationMode;
+import net.masterthought.cucumber.sorting.SortingMethod;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -11,6 +19,27 @@ import org.junit.runner.RunWith;
       "html:target/cucumber-report/",
       "json:target/cucumber-report/cucumber.json"
     },
-    tags = "@AC7.5",
+    //                tags = "@AC7.14",
     features = {"src/test/resources/Feature"})
-public class RunnerClass {}
+public class RunnerClass {
+
+  @AfterClass
+  public static void generateReport() {
+    System.out.println("The cucumber report is being generated. . .");
+    File reportOutputDirectory = new File("target/cucumberReport");
+    List<String> jsonFiles = new ArrayList();
+    jsonFiles.add("target/cucumber-report/cucumber.json");
+    String buildNumber = "v1.0";
+    String projectName = "Zhicong cucumber Report";
+    Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+    configuration.setBuildNumber(buildNumber);
+    configuration.addClassifications("Browser", "Firefox");
+    configuration.addClassifications("Branch", "release/1.0");
+    configuration.setSortingMethod(SortingMethod.NATURAL);
+    configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
+    ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+    reportBuilder.generateReports();
+    System.out.println(
+        "The cucumber report generated! Save in 'ZhicongWebAuto/target/cucumberReport/cucumber-html-reports' folder");
+  }
+}
